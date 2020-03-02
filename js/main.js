4c3a802712bb44663b066a59f3edb7cb251152f2
tarq-p <pierluigi.tarquinio90@gmail.com>
2020-03-02 13:12:27 +0100

    Accorgimenti

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ $(document).ready(function(){
 
     $('.menu-item').click(function(){
         if ($(this).find('.dropdown-menu').is(':hidden')) {
+            $('.menu-item').find('.dropdown-menu').slideUp();
             $(this).find('.dropdown-menu').slideToggle();
         } else {
             $(this).find('.dropdown-menu').slideToggle();
